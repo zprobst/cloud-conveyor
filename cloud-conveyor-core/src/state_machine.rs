dd2ce79568100e9985872cdc5d23f300c6e50b1a
Zach Probst <zprobst@resilientvitality.com>
2020-05-22 20:32:31 -0400

    clean up state machine

--- cloud-conveyor-core/src/state_machine.rs
+++ cloud-conveyor-core/src/state_machine.rs
@@ -12,56 +12,62 @@ struct StateMachine {
 
 impl StateMachine {
     pub fn new(pipeline: Pipeline) -> Self {
-        Self {
+        let mut result = Self {
             pipeline,
             current_action: None,
-        }
+        };
+        result.current_action = result.pipeline.pop_next_action();
+        result
     }
 
-    fn tick_machine_state(&mut self, context: &RuntimeContext) -> Result<bool, Error> {
+    /// Performs one cycle of the state machine by polling the current action's state. If the current
+    /// action is completed, the result is evaluated and any new works is added to the pipeline to
+    /// work on.
+    pub fn tick_machine_state(&mut self, context: &RuntimeContext) -> Result<bool, Error> {
         // Get the current action and see if it is done.
-        let mut start_action = match self.current_action {
-            Some(_) => false,
-            None => true,
+        if self.current_action.is_none() {
+            return Ok(true);
+        }
+
+        // Get the current state of the action.
+        let action = self
+            .current_action
+            .as_mut()
+            .expect("Action does not exist despite just checking its value");
+        let is_done = action.is_done(context)?;
+
+        // If the current action is still going, we can just bail here.
+        if !is_done {
+            return Ok(false);
+        }
+
+        // If the current action is done, we need to pop the next action and start it.
+        // If the pipeline is done, we can return that we are done.
+        let result = action.get_result(context);
+        let should_cancel_pending_actions = match result {
+            ActionResult::Success => false,
+            ActionResult::FailedAllow => false,
+            ActionResult::Failed => true,
+            ActionResult::Canceled => true,
         };
+        if should_cancel_pending_actions {
+            self.pipeline.cancel()
+        }
 
-        if !start_action {
-            let mut action = self
-                .current_action
-                .as_mut()
-                .expect("Action does not exist despite just checking its value");
-            let is_done = action.is_done(context)?;
-            if is_done {
-                start_action = true;
-                // We need to know the state of the job's result.
-                let result = action.get_result(context);
-
-                let cancel = match result {
-                    ActionResult::Success => false,
-                    ActionResult::FailedAllow => false,
-                    ActionResult::Failed => true,
-                    ActionResult::Canceled => true,
-                };
-
-                if cancel {
-                    self.pipeline.cancel()
-                }
-
-                // If there is new work, we will push these items
-                if let Some(actions) = action.get_new_work(context) {
-                    for action in actions {
-                        self.pipeline.add_immediate_action(action);
-                    }
-                }
+        // If there is new work, we will push these items onto the pipeline.
+        if let Some(actions) = action.get_new_work(context) {
+            for action in actions {
+                self.pipeline.add_immediate_action(action);
             }
         }
 
-        if start_action {
-            // Pop a new action.
-            // start it.
-            // poll its state right away (recursively)
+        // We will dequeue the next action and start it (if there is any).
+        self.current_action = self.pipeline.pop_next_action();
+        let has_remaining_actions = self.current_action.is_some();
+        if has_remaining_actions {
+            let action = self.current_action.as_mut().unwrap();
+            action.start(context)?;
         }
-
-        Ok(false)
+        Ok(has_remaining_actions)
     }
 }
